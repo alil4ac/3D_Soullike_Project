4dff8f3421ba56d478e2c303d64c59ed25ab469f
alil4ac <134047536+alil4ac@users.noreply.github.com>
2024-09-19 21:16:20 +0900

    5-Sound&Scene

---
+++ Sound/Monster_Fx.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster_Fx : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource MonsterFx;
+
+    private bool IsDone = false;
+
+    public void SetVolume(float volume)
+    {
+        MonsterFx.volume = 1f * volume;
+    }
+    public void StartSound(string soundName, float Volume = 1f)
+    {
+        MonsterFx.clip = GameSoundManager.Instance.audioClips[soundName];
+        StartCoroutine(_Play());
+    }
+
+    public void SoundSetting(float Volume = 1f)
+    {
+        MonsterFx.volume = Volume;
+    }
+
+    private IEnumerator _Play()
+    {
+        MonsterFx.Play();
+        while (true)
+        {
+            if (MonsterFx.isPlaying == false)
+            {
+                Destroy(this.gameObject);
+                yield break;
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
